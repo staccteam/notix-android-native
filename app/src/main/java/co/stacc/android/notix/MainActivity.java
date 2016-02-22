730a42700d37a71129b5fe502ab66615d454694b
Preetesh Jain <preeteshjain17@gmail.com>
2016-02-23 00:36:39 +0530

    Add Navigation Drawer
    Design and Functionality of Navigation Drawer added.

--- app/src/main/java/co/stacc/android/notix/MainActivity.java
+++ app/src/main/java/co/stacc/android/notix/MainActivity.java
@@ -1,40 +1,104 @@
 package co.stacc.android.notix;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-
-import static android.R.layout.simple_list_item_1;
-import static co.stacc.android.notix.R.layout.activity_main;
-import static co.stacc.android.notix.R.layout.content_main;
 
 public class MainActivity extends AppCompatActivity {
+
+    //First We Declare Titles And Icons For Our Navigation Drawer List View
+    //This Icons And Titles Are holded in an Array as you can see
+
+    String TITLES[] = {"Notifications", "Attachments", "Departments", "Syllabus"};
+    int ICONS[] = {
+            R.drawable.ic_social_notifications,
+            R.drawable.ic_file_attachment,
+            R.drawable.ic_social_school,
+            R.drawable.ic_editor_insert_drive_file
+    };
+
+    //Similarly we Create a String Resource for the name and email in the header view
+    //And we also create a int resource for profile picture in the header view
+
+    String NAME = "Preetesh Jain";
+    String SEMBRANCH = "8th Semester, CS Department";
+    int PROFILE = R.drawable.test_avatar;
+
+    private Toolbar toolbar;                              // Declaring the Toolbar Object
+
+    RecyclerView mRecyclerView;                           // Declaring RecyclerView
+    RecyclerView.Adapter mAdapter;                        // Declaring Adapter For Recycler View
+    RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+    DrawerLayout Drawer;                                  // Declaring DrawerLayout
+
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
 
 
-        ListView myListView = (ListView) findViewById(R.id.myListView);
+//        ListView myListView = (ListView) findViewById(R.id.myListView);
+//
+//        String[] names = {"Test 1", "Test 2"};
+//
+//        ListAdapter adapter = new ArrayAdapter<String>(this, simple_list_item_1, names);
+//        myListView.setAdapter(adapter);
 
-        String[] names = {"Test 1", "Test 2"};
+//        ImageView hamburgerIcon = (ImageView) findViewById(R.id.hamburgerIcon);
+//        hamburgerIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // Add code to bring up Navigation Bar
+//            }
+//        });
 
-        ListAdapter adapter = new ArrayAdapter<String>(this, simple_list_item_1, names);
-        myListView.setAdapter(adapter);
+        // Assigning the toolbar object of the view and setting the the Action bar to our toolbar
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView); // Assigning the RecyclerView Object to the xml View
+
+        mRecyclerView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+
+        mAdapter = new MyAdapter(TITLES,ICONS,NAME,SEMBRANCH,PROFILE);       // Creating the Adapter of MyAdapter class(which we are going to see in a bit)
+        // And passing the titles,icons,header view name, header view email,
+        // and header view profile picture
+
+        mRecyclerView.setAdapter(mAdapter);                              // Setting the adapter to RecyclerView
+
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+
+        mRecyclerView.setLayoutManager(mLayoutManager);                 // Setting the layout Manager
+
+
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);        // Drawer object Assigned to the view
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.app_name,R.string.app_name){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened( As I dont want anything happened whe drawer is
+                // open I am not going to put anything here)
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+
+        }; // Drawer Toggle Object Made
+        Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();               // Finally we set the drawer toggle sync State
+
     }
 
     @Override
@@ -58,4 +122,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
